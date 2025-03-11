ce798d15081e4d29f3edf88b967c2c920bfa508d
ons <elfekihons@gmail.com>
2025-03-11 23:37:27 +0100

    secondCommit

---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const cors = require("cors");
+const users=require("./routes/api/users");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const mongo_url = config.get("mongo_url");
+mongoose.set('strictQuery', true);
+mongoose
+.connect(mongo_url)
+.then(() => console.log("MongoDB connected..."))
+.catch((err) => console.log(err));
+app.use("/Users", users);
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Server running on port ${port}`));
