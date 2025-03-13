37c50a181a06dc6ffd8b1ec0a93b0e9f9c3a6bfa
ons <elfekihons@gmail.com>
2025-03-13 21:52:25 +0100

    changes

---
+++ frontend/src/components/home.js
@@ -0,0 +1,8 @@
+const Home = () => {
+    return (
+    <div className="flex items-center justify-center h-screen">
+    <p className="text-2xl font-bold">Hello</p>
+    </div>
+    );
+    };
+    export default Home;
---
+++ frontend/src/components/register.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { TextField, Button, Box, Typography, MenuItem, Select, FormControl, InputLabel } from "@mui/material";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [message, setMessage] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/users/register",
+        formData
+      );
+      setMessage(response.data.msg);
+      setFormData({ username: "", password: ""}); // Réinitialisation des champs
+    } catch (error) {
+      setMessage(error.response?.data?.msg || "Une erreur est survenue");
+    }
+  };
+
+  return (
+    <Box
+sx={{
+maxWidth: 400,
+mx: "auto",
+mt: 5,
+p: 3,
+boxShadow: 3,
+borderRadius: 2,
+backgroundColor: "white",
+}}
+>
+<Typography variant="h5" mb={2} textAlign="center">
+Inscription
+</Typography>
+<form onSubmit={handleSubmit}>
+<TextField
+fullWidth
+label="Nom d'utilisateur"
+name="username"
+value={formData.username}
+onChange={handleChange}
+margin="normal"
+required
+/>
+<TextField
+fullWidth
+type="password"
+label="Mot de passe"
+name="password"
+value={formData.password}
+onChange={handleChange}
+margin="normal"
+required
+/><Typography textAlign="center" mt={2}>
+  Déjà un compte ?{" "}
+  <a href="/login" style={{ color: "blue", textDecoration: "none" }}>
+    Connectez-vous ici
+  </a>
+</Typography>
+
+<Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
+S'inscrire
+</Button>
+</form>
+{message && (
+<Typography color="error" textAlign="center" mt={2}>
+{message}
+</Typography>
+)}
+</Box>
+);
+};
+export default Register;
+
